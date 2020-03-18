5e7a29c7e4267aba88728f29bd8312b5a64dcdb4
xzarra <aryasatya6969@gmail.com>
2020-03-18 14:50:10 -0400

    menambahkan folder operator aritmatika dan membuat file program kalkulator sederhana toko buku dengan operator aritmatika

---
+++ operator aritmatika/aritmatika.py
@@ -0,0 +1,15 @@
+# operator aritmatika 
+
+# bayangkan, saat ini kita sedang di toko buku
+dompetSaya = 100000
+# misalkan kita mempunyai uang 100.000
+totalBelanja = 75000
+# harga buku yang akan kita beli berharga 75.000
+pajak = totalBelanja * 10 / 100
+# terdapat pajak 10% pada setiap total belanja
+hargaTotal = totalBelanja + pajak
+# maka harga total yang harus kita beli adalah total belanja ditambah biaya pajak
+print("uang yang ada di dompet saya  :",dompetSaya) # menampilkan variabel dompetSaya
+print("harga buku                    :",totalBelanja) # menampilkan variabel totalBelanja
+print("total pajak                   :",pajak) # menampilkan variabel pajak
+print("harga buku yang sudah dipajak :",hargaTotal) # menampilkan variabel hargaTotal
