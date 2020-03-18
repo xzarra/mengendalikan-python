69b5029820c5f64099e16f995240d035b98779b2
xzarra <aryasatya6969@gmail.com>
2020-03-18 12:27:15 -0400

    menambahkan folder data variable dan membuat contoh variable

---
+++ data variable/var.py
@@ -0,0 +1,6 @@
+#belajar variable
+# kumpulan data secara acak
+# membuat variable (text atau String)
+
+hewan = "ayam, sapi, katak, kambing"#ini adalah variable
+buah = "anggur"#ini adalah variable
