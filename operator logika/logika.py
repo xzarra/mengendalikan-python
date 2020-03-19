f637b3b4af11e9e465132684a3ad73a5d389ee5e
xzarra <aryasatya6969@gmail.com>
2020-03-19 22:24:19 +0700

    menambah folder operator logika dan membuat contoh program validasi password

--- operator logika/logika.py
+++ operator logika/logika.py
@@ -7,4 +7,4 @@ comfirmPassword = "arya1234" # misalnya kita memasukkan typo
 
 # validasi password, apakah password sama dengan confirm password atau tidak
 # kita samakan password dan validasi apakah sama ataukah tidak
-print(password == "arya123" and comfirmPassword == "arya123") # hasilnya adalah false karena tidak sama dengan yang di validasi
+print(password == "arya123" and comfirmPassword == password) # hasilnya adalah false karena tidak sama dengan yang di validasi
