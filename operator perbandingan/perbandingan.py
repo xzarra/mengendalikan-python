bdcefe66a8414c8bd78b58d175ed9d844119b45f
xzarra <aryasatya6969@gmail.com>
2020-03-19 22:08:33 +0700

    menambahkan folder operator perbandingan dan membuat contoh program autentikasi data sederhana

---
+++ operator perbandingan/perbandingan.py
@@ -0,0 +1,11 @@
+# operator perbandingan
+
+# contoh autentikasi akun
+username = "Arya Satya"
+password = "Arya6969"
+
+# validasi apakah terdapat di database atau tidak
+dataBase = "Arya Satya Saputra"
+
+# menampilkan keluaran, apakah bernilai benar atau salah
+print(username == dataBase)
