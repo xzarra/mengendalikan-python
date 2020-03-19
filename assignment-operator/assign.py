978c036b57d704e54288776a8cc1191e7c8ed413
xzarra <aryasatya6969@gmail.com>
2020-03-19 20:51:14 +0700

    menambahkan folder Assignment operator dan membuat contoh program assignment operator

---
+++ assignment-operator/assign.py
@@ -0,0 +1,10 @@
+# inisialisasi variable umur
+
+# Contoh, umur kita adalah 30
+umur = 30
+
+umur += 20 # lalu kita tamba 20
+
+umurTotal = umur/2 # dan dibagi menjadi dua
+
+print(umurTotal) # print umur total
