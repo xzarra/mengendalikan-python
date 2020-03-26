f6fc46ce09604c0661006b2dee3cb454e2a4f6ed
xzarra <aryasatya6969@gmail.com>
2020-03-26 20:10:43 +0700

    membuat folder menghitung list dan membuat file cara menghitung list array

---
+++ menghitung list/list.py
@@ -0,0 +1,8 @@
+# menghitung panjang list :)
+
+warna = ["merah","hitam","kuning","hijau"]
+# jumlah array akan dihitung di keluaran
+print(len(warna)) # outputnya adalah 4
+# kita coba mencari berapa karakter yang ada di array ke 2
+# yaitu kuning
+print(len(warna[2]))
