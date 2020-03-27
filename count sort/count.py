13658023f11846e7d8bc1e20f7d4017be2ac2a39
xzarra <aryasatya6969@gmail.com>
2020-03-27 17:14:43 +0700

    menambah folder count & sort dan membuat file menghitung data dan mengurutkan data

---
+++ count sort/count.py
@@ -0,0 +1,12 @@
+# count adalah menghitung data
+
+menu = ["nasi goreng","mi ayam","aneka gorengan","bakso","bakso"]
+
+# cara menghitung data adalah menggunakan . count
+menuTotal = menu.count("bakso")
+# kita hitung berapa banyak data bakso dimenu
+print(menuTotal) # outputnya adalah 2 karena data bakso ada dua
+
+menuNasi = menu.count("nasi goreng")
+# kita coba hitung ada berapa nasi goreng pada data
+print(menuNasi) 
---
+++ count sort/sort.py
@@ -0,0 +1,16 @@
+# sort adalah mengurutkan data
+
+menu =["ayam","ikan","gajah","jerapah"]
+# kita inisialisi variabel
+
+menu.sort()
+# kita mengurutkan dari alfabet a
+
+print(menu) # outputnya adalah ayam, gajah, ikan, jerapah
+
+# kita coba buat mengurutkan dari alfabet z
+data = ["ayam","ikan","gajah","jerapah"]
+
+data.sort(reverse=True)
+
+print(data)
