7fe13b9b36af75f5192c1703d05ab1b450684d72
xzarra <aryasatya6969@gmail.com>
2020-04-11 23:33:15 +0700

    menambah folder fungsi dan membuat file fungsi, dan mendeklarasikan fungsi input

---
+++ fungsi/fungsi.py
@@ -0,0 +1,14 @@
+# membuat fungsi
+# fungsi adalah blok kode terorganisir dan dapat digunakan kembali yang digunakan untuk melakukan sebuah tindakan / action. -belajarpython.com
+# fungsi blok dimulai dengan def lalu diikuti nama fungsi dan tanda kurung dan tanda titik dua
+
+# contoh fungsi
+def printme(str): # kita buat fungsi diawali dengan def
+    str = " ini adalah fungsi pertamaku"
+    print(str)
+    return
+
+printme(str) # melakukan pemanggilan fungsi diatas
+
+x = input("input : ")
+print(x)
