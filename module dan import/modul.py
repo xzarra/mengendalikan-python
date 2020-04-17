45b88aaa4f16d920029a5a5fd704c31380c2fc44
xzarra <aryasatya6969@gmail.com>
2020-04-18 00:37:13 +0700

    menambah folder module dan import lalu mengapilkasikan fungsi import

---
+++ module dan import/modul.py
@@ -0,0 +1,10 @@
+# di modul ini kita bisa tuliskan data, fungsi, atau output(keluaran)
+# modul bisa di import ke main.py
+# contoh penggunaan modul dan import
+
+data = "Ini variabel" # ini variabel
+
+print("ini adalah keluaran atau output") # ini adalah keluaran
+
+def cek(): # kita inisialisasikan fungsi
+    print("ini fungsi dari cek") # kita tampilkan keluaran
