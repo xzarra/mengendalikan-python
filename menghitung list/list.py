f7ea75f7cb36555e5bd8bd468eb8f140c5f7cdb9
xzarra <aryasatya6969@gmail.com>
2020-04-17 15:15:40 +0700

    menambah output program di list.py

--- menghitung list/list.py
+++ menghitung list/list.py
@@ -5,4 +5,4 @@ warna = ["merah","hitam","kuning","hijau"]
 print(len(warna)) # outputnya adalah 4
 # kita coba mencari berapa karakter yang ada di array ke 2
 # yaitu kuning
-print(len(warna[2]))
+print(len(warna[2])) # hasilnya adalah 6, karena kuning terdiri dari 6 huruf
