aed0c7e69ab2bf7765d9521d2103eeac7386ae7f
xzarra <aryasatya6969@gmail.com>
2020-04-18 20:14:15 +0700

    memodifikasi main

--- module dan import/main.py
+++ module dan import/main.py
@@ -1,5 +1,16 @@
+# import modul.py
 import modul
+# secara default modul akan menampilkan keluaran dahulu
 
-print(modul.data)
+print(modul.data)# kita panggil keluaran atau variabel dari data
 
-modul.cek()
+modul.cek() # kita panggil keluaran atau output dari fungsi cek
+
+''' 
+outputnya adalah
+
+ini adalah keluaran atau output
+Ini variabel
+ini fungsi dari cek
+
+'''
